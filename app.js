df7cac3f5f31be45323786b2ff06d6653792e4f0
sebnun <sebnun@gmail.com>
2016-09-09 18:35:15 -0300

    implemented all user cases

--- app.js
+++ app.js
@@ -1,92 +1,66 @@
-const http = require("http");
-const MongoClient = require('mongodb').MongoClient
-
-const url = 'mongodb://localhost:27017/mydb';
-
-http.createServer(function (req, res) {
-
-    if (req.url === "/" || req.url === "//") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(`<p>See <a href="https://github.com/sebnun/shortener">Shortener</a> for more info.</p>`)
-
-    } else if (req.url.substring(0, 6) === "//new/") { //if startrts with /new/
-
-        //req.url withou new 
-        const userURL = decodeURI(req.url.substring(6));
-        let response = null;
-
-        if (isUrl(userURL)) {
-
-            //create unique id
-            const hash = hashCode(userURL);
-
-            MongoClient.connect(url, function (err, db) {
-
-                db.collection('links').insertOne({ "link": hash, "url": userURL }, function (err, r) {
-                    db.close();
-
-                    //make respnse json
-                    response = {
-                        "original_url": userURL,
-                        "short_url": `https://injavascript.com/shortener/${hash}`
-                    };
-                    res.writeHead(200, { "Content-Type": "text/json" });
-                    res.end(JSON.stringify(response));
-                });
-            });
-
-
-        } else {
-            response.error = "Wrong url format, make sure you have a valid protocol and real site.";
-            res.writeHead(200, { "Content-Type": "text/json" });
-            res.end(JSON.stringify(response));
-        }
-
-
-
-    } else {
-
-        const userLink = decodeURI(req.url.substring(2));
-        let response = null;
-
-        if (url === null) {
-
-            response.error = "This url is not on the database.";
-            res.writeHead(200, { "Content-Type": "text/json" });
-            res.end(JSON.stringify(response));
-        } else {
-
-            MongoClient.connect(url, function (err, db) {
-
-                var col = db.collection('links');
-
-                col.find({ "link": userLink }).limit(1).toArray(function (err, docs) {
-                    db.close();
-
-                    res.statusCode = 302;
-                    res.setHeader('Location', url);
-                    res.end();
-                });
-            });
-
-        }
-
-    }
-
-}).listen(8082);
-
 function isUrl(s) {
-    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
-    return regexp.test(s);
+  return /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+    .test(s);
 }
 
-function hashCode(str) {
-    var hash = 0;
-    if (str.length == 0) return hash;
-    for (i = 0; i < str.length; i++) {
-        char = str.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash; // Convert to 32bit integer
+const http = require('http');
+const url = require('url');
+const MongoClient = require('mongodb').MongoClient;
+const shortid = require('shortid');
+
+const dburl = 'mongodb://localhost:27017/mydb';
+
+http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url);
+
+  if (parsedUrl.path === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<p>See <a href="https://github.com/sebnun/shortener">Shortener</a> for more info.</p>');
+  } else if (/^\/new\/\S+/.test(parsedUrl.path)) { // matches /new/jgjhg56
+    const userUrl = parsedUrl.path.substring(5);
+
+    if (isUrl(userUrl)) {
+      MongoClient.connect(dburl, (err, db) => {
+        if (err) throw err;
+
+        const sid = shortid.generate();
+
+        db.collection('links').insertOne({ url: userUrl, id: sid }, (inserterr) => {
+          if (inserterr) throw err;
+          db.close();
+          const linkjson = {
+            original_url: userUrl,
+            short_url: `https://injavascript.com/shortener/${sid}`,
+          };
+
+          res.writeHead(200, { 'Content-Type': 'text/json' });
+          res.end(JSON.stringify(linkjson));
+        });
+      });
+    } else {
+      const e = { error: 'Wrong url format, make sure you have a valid protocol and real site.' };
+      res.writeHead(200, { 'Content-Type': 'text/json' });
+      res.end(JSON.stringify(e));
     }
-    return hash;
-}
+  } else if (parsedUrl.path !== '/favicon.ico') {
+    const linkid = parsedUrl.path.substring(1);
+
+    MongoClient.connect(dburl, (err, db) => {
+      if (err) throw err;
+
+      db.collection('links').find({ id: linkid }).toArray((finderr, result) => {
+        if (finderr) throw finderr;
+
+        if (result.length === 0) {
+          res.writeHead(200, { 'Content-Type': 'text/json' });
+          res.end(JSON.stringify({ error: 'This url is not on the database.' }));
+        } else {
+          res.writeHead(302, { Location: result[0].url });
+          res.end();
+        }
+
+        db.close();
+      });
+    });
+  }
+}).listen(8082);
